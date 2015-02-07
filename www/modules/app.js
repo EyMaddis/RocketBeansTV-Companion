ce279f5fa012d685747672073ed6f63c0f668f73
Maddis <maddis@kaleydra.de>
2015-02-07 03:15:26 +0100

    caching, twitter and twitch on home screen

--- www/modules/app.js
+++ www/modules/app.js
@@ -4,47 +4,82 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('rbtv', ['ionic', 'rbtv.main', 'rbtv.home', 'rbtv.schedule', 'rbtv.stream', 'rbtv.shows', 'rbtv.redditbeans', 'restangular', 'rbtv.notification'])
-
-.run(['$ionicPlatform', '$rootScope', '$ionicHistory', function($ionicPlatform, $rootScope, $ionicHistory) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-    if(ionic.Platform.isAndroid()){
-      $ionicPlatform.registerBackButtonAction(function (event) {
-        if($ionicHistory.currentStateName() == "myiew"){
-          ionic.Platform.exitApp();
-          // or do nothing
-        }
-        else {
-          $ionicHistory.goBack();
-        }
-      }, 100);
-    }
-    // TODO
-    $rootScope.imageBaseURL = 'http://192.168.178.25:3001/images/show-logos/';
 
 
-  });
+
+angular.module('rbtv', [
+    'ionic',
+    'angular-data.DSCacheFactory',
+    'ImgCache',
+    'rbtv.main',
+    'rbtv.home',
+    'rbtv.schedule',
+    'rbtv.stream',
+    'rbtv.shows',
+    'rbtv.redditbeans',
+    'restangular',
+    'rbtv.notification'
+])
+
+
+.config(['RestangularProvider', 'ImgCacheProvider', '$urlRouterProvider', function(Restangular, ImgCacheProvider, $urlRouterProvider){
+    Restangular.setBaseUrl("http://192.168.178.25:3001/api/"); //TODO configurable for production
+
+    // or more options at once
+    ImgCacheProvider.setOptions({
+        debug: true,
+        usePersistentCache: true
+    });
+
+    ImgCacheProvider.manualInit = true;
+
+    $urlRouterProvider.otherwise('/home');
 }])
+.run([
+    '$ionicPlatform',
+    '$rootScope',
+    '$ionicHistory',
+    '$http',
+    'Restangular',
+    'DSCacheFactory',
+    'ImgCache',
+    function($ionicPlatform, $rootScope, $ionicHistory, $http, Restangular, DSCacheFactory, ImgCache) {
+        $ionicPlatform.ready(function() {
+            ImgCache.$init();
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+            if(ionic.Platform.isAndroid()){
+                $ionicPlatform.registerBackButtonAction(function (event) {
+                    if($ionicHistory.currentStateName() == "myiew"){
+                        ionic.Platform.exitApp();
+                        // or do nothing
+                    }
+                    else {
+                        $ionicHistory.goBack();
+                    }
+                }, 100);
+            }
+            // TODO
+            $rootScope.imageBaseURL = 'http://192.168.178.25:3001/images/show-logos/';
 
-.config(['RestangularProvider', function(Restangular){
-      Restangular.setBaseUrl("http://192.168.178.25:3001/api/"); //TODO configurable for production
-      Restangular.setDefaultHttpFields({
-        timeout: 5000,
-        cache: true
-      });
-}])
-
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
-}]);
+            console.log(DSCacheFactory);
+            DSCacheFactory('defaultCache', {
+                maxAge: 900000, // Items added to this cache expire after 15 minutes.
+                cacheFlushInterval: 6000000, // This cache will clear itself every hour.
+                //deleteOnExpire: 'aggressive', // Items will be deleted from this cache right when they expire.
+                storageMode: 'localStorage' // This cache will sync itself with `localStorage`.
+            });
+            $http.defaults.cache = DSCacheFactory.get('defaultCache');
+            Restangular.setDefaultHttpFields({
+                timeout: 5000,
+                cache: DSCacheFactory.get('defaultCache')
+            });
+        });
+    }])
--- www/modules/home/controllers/home.js
+++ www/modules/home/controllers/home.js
@@ -2,13 +2,15 @@ angular.module('rbtv.home').controller('HomeController', ['$scope', '$state', '$
     $http.jsonp('https://api.twitch.tv/kraken/streams/rocketbeansTV?callback=JSON_CALLBACK')
         .success(function(data){
             $scope.twitchStream = data.stream;
+            twttr.widgets.load();
         })
         .error(function(err){
+            twttr.widgets.load(); // TODO
             console.error('twich api error',err);
         });
-
 
     $scope.watchNow = function(){
         $state.go('rbtv.stream');
     };
+
 }]);
